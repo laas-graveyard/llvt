3268c4b2a88aebd3b5422fe716650d2b7da2fa89
Thomas Moulard <thomas.moulard@gmail.com>
2010-10-02 00:16:18 +0200

    Add license header.

--- include/llvs/tools/CORBAReference.h
+++ include/llvs/tools/CORBAReference.h
@@ -1,9 +1,21 @@
-/*! \brief 
- * \author Olivier Stasse
- * Copyright:  JRL, CNRS-AIST.
- * Date: 12/08/2008
+/*
+ * Copyright 2010, CNRS/AIST, Clément Petit
+ *
+ * This file is part of llvt.
+ * llvt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * llvt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with llvt.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef _DHFSM_CORBA_REFERENCE_H_ 
+
+#ifndef _DHFSM_CORBA_REFERENCE_H_
 #define _DHFSM_CORBA_REFERENCE_H_
 
 #include <string>
@@ -18,10 +30,10 @@ namespace LLVClient
   class CORBAReference
     {
     public:
-      
+
        /*! \brief Default constructor */
       CORBAReference();
-      
+
       /*! \brief Default destructor */
       ~CORBAReference();
 
@@ -29,7 +41,7 @@ namespace LLVClient
       bool InitORB();
 
       /*! \brief Returns the reference to a CORBA object. */
-      CORBA::Object_ptr getObjectReference(std::string ServerID, 
+      CORBA::Object_ptr getObjectReference(std::string ServerID,
 					   std::string ServerKind);
 
       /*! \brief setting the hostname name service. */
@@ -40,7 +52,7 @@ namespace LLVClient
 
       /*! \brief setting the port number. */
       bool SetPortNumber(unsigned short &PortNumber);
-      
+
       /*! \brief getting the port number. */
       const unsigned short & GetPortNumber() const;
 
@@ -51,7 +63,7 @@ namespace LLVClient
 
       /*! \brief Store the port number. */
       unsigned short m_PortNumber;
-      
+
       /*! \brief Store the hostname. */
       std::string m_HostName;
 
--- include/llvs/tools/Debug.h
+++ include/llvs/tools/Debug.h
@@ -1,12 +1,19 @@
-/** @doc Default header for debug mode
-    
-    Copyright (c) 2010, 
-    @author Stephane Embarki
-   
-    JRL-Japan, CNRS/AIST
-    
-    See license file for information on license.
-*/
+/*
+ * Copyright 2010, CNRS/AIST, Clément Petit
+ *
+ * This file is part of llvt.
+ * llvt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * llvt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with llvt.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef _DEBUG_HEADER_LLVS_H_
 
@@ -14,14 +21,14 @@
 
 #define ODEBUG2(x)
 #define ODEBUG3(x) std::cerr << __FILE__ << ": l" << __LINE__ << ": " << x << std::endl
-#define ODEBUG3_CONT(x) cerr << x 
+#define ODEBUG3_CONT(x) cerr << x
 
 #ifdef _DEBUG_MODE_ON_
  #define ODEBUG(x) std::cerr << __FILE__ << ": l" << __LINE__ << ": " << x << std::endl
  #define ODEBUG_CONT(x) std::cerr << __FILE__ << ": l" << __LINE__ << ": " << x << std::endl
 #else
- #define ODEBUG(x) 
- #define ODEBUG_CONT(x) 
+ #define ODEBUG(x)
+ #define ODEBUG_CONT(x)
 #endif
 
 #endif /* _DEBUG_HEADER_LLVS_H_ */
