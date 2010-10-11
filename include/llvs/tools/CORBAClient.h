4d48edc15f0aa641b15366c88d786e6942859a2a
Thomas Moulard <thomas.moulard@gmail.com>
2010-10-11 14:30:24 +0200

    Remove trailing white-space.

--- include/llvs/tools/CORBAClient.h
+++ include/llvs/tools/CORBAClient.h
@@ -15,7 +15,7 @@
  * along with llvt.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _CORBA_FOR_LLVS_CLIENT_H 
+#ifndef _CORBA_FOR_LLVS_CLIENT_H
 #define _CORBA_FOR_LLVS_CLIENT_H
 
 #include <llvs/tools/CORBAReference.h>
@@ -37,30 +37,30 @@ namespace LLVClient
 
     /*! \brief Get the name service. */
     const std::string & GetServiceName() const;
-	    
+
     /*! \brief Get the service's kind. */
     const std::string & GetServiceKind() const;
-	    
-	    
+
+
     /*! \brief Get the CORBA reference. */
     LLVClient::CORBAReference* getCORBAReference() const;
-	    
+
     /*! \brief Set the CORBA reference. */
     void setCORBAReference(CORBAReference * aCorbaRef);
-	    
-    /*! \brief Connection to the server. 
+
+    /*! \brief Connection to the server.
      */
     template<class DataType, class DataType_var>
       DataType_var GetServicePort(const std::string& ServiceName = DEFAULT_LLVS_SERVICE_NAME,
 				  const std::string& ServiceKind = DEFAULT_LLVS_SERVICE_KIND) throw(const char*);
-  
+
   protected:
     /*! \brief Store the service name (set by default to LLVS one). */
     std::string m_ServiceName;
-  	    
+
     /*! \brief Store the service kind (set by default to LLVS one). */
     std::string m_ServiceKind;
-  	  
+
     /*! Pointer to the CORBA reference object. */
     LLVClient::CORBAReference* m_CORBAReference;
   };
